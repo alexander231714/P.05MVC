7da2c545d3e17e646f3796130053748d32a410ca
vladi <vladi@LAPTOP-MQML25RF>
2023-03-31 18:45:04 -0600

    Agregar archivos de proyecto.

---
+++ MVCpracticauno/Models/equiposDbContex.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace MVCpracticauno.Models
+{
+    public class equiposDbContex : DbContext
+    {
+        public equiposDbContex(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<marcas> marcas { get; set; }
+    }
+}
